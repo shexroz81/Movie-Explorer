72c031a5e61555a0569dee313595d7804d84c0cb
shexroz <shexroz81@gmail.com>
2026-04-03 17:15:19 +0500

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,42 @@
+const API_KEY = "d96b6507";
+const form = document.querySelector(".search-form");
+const input = document.querySelector("#movie-search");
+
+async function getMovie(movieName) {
+  const url = `https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieName}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    // Mavjud container ni topish yoki yangisini yaratish
+    let container = document.querySelector(".movie-container");
+    if (!container) {
+      container = document.createElement("div");
+      container.className = "movie-container";
+      document.body.appendChild(container);
+    }
+
+    const moviePoster = document.createElement("div");
+    moviePoster.className = "movie-poster";
+    moviePoster.innerHTML = `
+       <h2>${data.Title}</h2>
+       <p>${data.Year}</p>
+      <img src="${data.Poster}" alt="${data.Title}">
+    `;
+
+    container.appendChild(moviePoster);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const movieName = input.value.trim();
+  if (movieName) {
+    getMovie(movieName);
+  }
+  form.reset();
+});
